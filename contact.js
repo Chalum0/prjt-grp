3ad965f9c665c308dcbbbb6abb1a6492e44ab221
Yann <yann.lestage@gmail.com>
2024-01-19 18:18:27 +0100

    bah j'ai fini

--- contact.js
+++ contact.js
@@ -2,20 +2,16 @@ const SendButton = document.querySelector("#send_button");
 const FormNom = document.querySelector("#nom");
 const FormPrenom = document.querySelector("#Prenom");
 const FormEmail = document.querySelector("#email");
-let TextError = document.querySelector("#text_error");
+let TextError = document.querySelector("#text_button_click");
 
 SendButton.addEventListener("click", (event) => {
     if (FormNom.value === "" || FormPrenom.value === "" || FormEmail.value === "") {
-        TextError.textContent = "Veuillez remplir toutes les cases avant d'envoyer"
+        TextError.textContent = "Veuillez remplir toutes les cases avant d'envoyer";
     }
     else {
-        TextError.textContent = "Informations envoyé avec succés"
+        TextError.textContent = "Informations envoyé avec succés";
+        FormNom.value = "";
+        FormPrenom.value = "";
+        FormEmail.value = "";
     }
 });
-
-function storage (){
-let item = JSON.parse(localStorage.getItem("item")) // récupérer la donnée
-
-const data = JSON.stringify(item); // transformer l'item en json
-localStorage.setItem("item", data);  // sotcker l'item
-}
